c11218c9c0c5e7048e8ff52f2b6d321b20cba131
sakshinikharge39 <srnikharge7788@gmail.com>
2024-02-09 21:54:25 +0530

    Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,51 +1,131 @@
-import React, { useState } from 'react'
-import './Card.css'
-import {AnimateSharedLayout}  from
-'framer-motion'
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import React, { useState } from "react";
+import "./Card.css";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { UilTimes } from "@iconscout/react-unicons";
+import Chart from 'react-apexcharts'
 
-const Card= (props) =>{
-
-  const [expended, setexpended] = useState(false)
+const Card = (props) => {
+  const [expended, setExpended] = useState(false);
   return (
     <AnimateSharedLayout>
-      {
-        expended ? (
-          'Expanded'
-        ):
-        <CompactCard param = {props}/>
-      }
-
+      {expended ? (
+        <ExpendedCard
+          param={props}
+          setExpended={() => {
+            setExpended(false);
+          }}
+        />
+      ) : (
+        <CompactCard param={props} setExpended={() => setExpended(true)} />
+      )}
     </AnimateSharedLayout>
-  )
-}
+  );
+};
 
 //compactCard
-function CompactCard({param}){
+function CompactCard({ param, setExpended }) {
   const Png = param.png;
-  return(
-    <div className="CompactCard"
-    style={{
-      background : param.color.backGround,
-      boxShadow : param.color.boxShadow
-    }}>
+  return (
+    <motion.div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      onClick={setExpended}
+      layoutId="expandableCard"
+    >
       <div className="radialBar">
-        <CircularProgressbar value={param.barValue}
-        text={`${param.barValue}%`}
-        /> 
+        <CircularProgressbar
+          value={param.barValue}
+          text={`${param.barValue}%`}
+        />
         <span>{param.title}</span>
       </div>
       <div className="detail">
-        <Png/>
+        <Png />
         <span>${param.value}</span>
         <span>Last 24 hours</span>
       </div>
-    </div>
-  )
+    </motion.div>
+  );
 }
 
+//ExpendedCard
+function ExpendedCard({ param, setExpended }) {
 
+  const data = {
+    options:{
+      chart : {
+        type:'area',
+        height: "auto"
+      },
 
+      dropShadow:{
+        enabled: false,
+        enabledOnSeries: undefined,
+        top: 0,
+        left :0,
+        blur:3,
+        color: "#000",
+        opacity:'0.35%',
+      },
 
-export default Card
+      fill:{
+        colors: ["#fff"],
+        type : "gradient",
+      },
+      dataLabels:{
+        enabled: false,
+      },
+      stroke:{
+        curve: "smooth",
+        colors: ["white"],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+      grid: {
+        show: true,
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
+      }
+    }
+  }
+  return (
+    <motion.div
+      className="ExpendedCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+    >
+      <div style={{alignSelf : 'flex-end', cuser:'pointer', color:'white'}}>
+        <UilTimes onClick={setExpended}
+        />
+      </div>
+      <span>{param.title}</span>
+      <div className="chartContainer">
+        <Chart series = {param.series} type='area' options = {data.options}/>
+      </div>
+      <span>Last 24 hours</span>
+    </motion.div>
+  );
+}
+
+export default Card;
